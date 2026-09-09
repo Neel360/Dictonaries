ad41bcff3f792211dc4af7204db28ce8312b3439
neel-guckool <neel001g@gmail.com>
2026-09-09 16:43:22 +0100

    frequency.py

---
+++ country_code.py
@@ -0,0 +1,14 @@
+country_code = {
+"india": "0091",
+"usa": "001",
+"canada": "001",
+"uk": "0044",
+"australia": "0061",
+}
+
+
+print("Country Code for india -")
+print(country_code.get("india", "Not Found"))
+
+print("Country Code for japan -")
+print(country_code.get("japan", "Not Found"))
